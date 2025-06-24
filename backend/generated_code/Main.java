90823fea860742e376a27f367c51d908528585dd
Shagun Verma <shagun252004@gmail.com>
2025-06-24 11:26:03 +0530

    Delete backend/generated_code directory

--- backend/generated_code/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int temperature = 10;
-        if (temperature > 30) {
-            System.out.println("temperature is greater than 30");
-        }
-    }
-}
