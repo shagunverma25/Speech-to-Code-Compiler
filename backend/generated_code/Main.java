3cae13d6c666bcb5cbd8377cf912b1800d21e09d
Shagun Verma <shagun252004@gmail.com>
2025-06-24 11:38:11 +0530

    Add files via upload

---
+++ backend/generated_code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int temperature = 10;
+        if (temperature > 30) {
+            System.out.println("temperature is greater than 30");
+        }
+    }
+}
