fa0f21c91a5bb5e96aef6fff892679bc6764f813
Shagun Verma <shagun252004@gmail.com>
2025-06-25 16:55:59 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainUI from './components/MainUI';
+
+function App() {
+  return (
+    <div className="App">
+      <MainUI />
+    </div>
+  );
+}
+
+export default App;
